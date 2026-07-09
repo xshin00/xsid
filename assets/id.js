e6abcfba5d13daf11be711953b9c819bfadc1389
shino <93799705+xshin00@users.noreply.github.com>
2026-07-09 23:01:11 +0200

    Change home redirection URL format

--- assets/id.js
+++ assets/id.js
@@ -14,7 +14,7 @@ if (date.getHours() >= 18){
 document.querySelector(".welcome").innerHTML = welcome;
 
 function toHome(){
-    location.href = '/home.html' + params;
+    location.href = '/home?' + params;
 }
 
 var input = document.querySelector(".password_input");
